cbbb1592b10d145ceead3a80ef26dc25bca72f24
Gambo2907 <67343582+Gambo2907@users.noreply.github.com>
2020-07-31 12:45:31 -0600

    Botón de eliminar y nueva tabla
    Añadí el botón de eliminar datos en la tabla de los servicios añadidos, le añadí otra tabla en la base de datos para que esta tenga los servicios añadidos.
    pd: no he podido encontrar la manera de añadir los servicios a la otra tabla, si encuentra algo lo puede implementar

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ class productos:
 
 	#Almacenando la base de datos dentro de la aplicación
 	db_name = "database.db"
-
 	#Programando el inicio y display de la aplicación
 	def __init__(self,window):
 		self.wind=window
@@ -34,6 +33,14 @@ class productos:
 		self.bservicios = ttk.Button(frame,text='Anadir servicio')#,command=self.add_service)
 		self.bservicios.grid(row=2,columnspan=3,sticky=W+E)
 
+		#Botón de eliminar servicio
+		self.delete = ttk.Button(frame, text = "Eliminar servicio",command=self.delete_service)
+		self.delete.grid(row=3,columnspan=4,sticky=W+E)
+		
+		#Mensajes
+		self.message = Label(text = "", fg= "black")
+		self.message.grid(row=3, columnspan=5, sticky=W+E)
+		
 		#Creando la tabla de servicios con su respectivo id
 		self.tree=ttk.Treeview(height=10,columns=2)
 		self.tree.grid(row=1,column=0,columnspan=2,pady=5,padx=5)
@@ -51,17 +58,18 @@ class productos:
 		self.treec.heading("#0", text="Nombre", anchor=CENTER)
 		self.treec.heading("#1", text="Precio", anchor=CENTER)
 
-
 		#Colocando los productos en la tabla de servicios
 		self.get_product()
-
+		self.get_service()
+	
 	#Llamando a la base de datos
 	def run_query(self,query,parameters = ()):
 		with sqlite3.connect(self.db_name) as conn:
 			cursor = conn.cursor()
 			result = cursor.execute(query, parameters)
 			conn.commit()
-		return result 
+		return result  
+
 	#Obteniendo los productos de la base de datos
 	def get_product(self):
 		#Limpiando los datos que están en la tabla
@@ -73,25 +81,61 @@ class productos:
 		#Recorriendo la lista de productos para insertarlos en la tabla
 		for row in db_rows:
 			self.tree.insert("", 0, text= row[1],values=row[2])
+	
+	#Obtienendo la base de datos de la segunda tabla
+	def get_service(self):
+		#Limpiando los datos que están en la tabla
+		records = self.treec.get_children()
+		for element in records:
+			self.treec.delete(element)
+		query = "SELECT * FROM Services ORDER BY Nombre DESC"
+		db_rows = self.run_query(query)
+		#Recorriendo la lista de productos para insertarlos en la tabla
+		for row in db_rows:
+			self.treec.insert("", 0, text= row[1],values=row[2])
+	
+	#def validation(self):
+		#if len(self.service.get())!=0:
+	 		#return True
+		#else:
+            #return False
 
-	# def validation(self):
-	# 	if len(self.service.get())!=0:
-	# 		return True
-	# 	else:
-	# 		return False
-
-	# def add_service(self):
-	# 	if self.validation()==True:
-	# 		query = "INSERT INTO product VALUES(NULL,?)"
-	# 		parameters=(self.service.get())
-	# 	else:
-	# 		print ("Ponga el nombre del servicio")
-	# 	self.add_service()
-
-
-
-
-
+	#def add_service(self):
+		#if self.validation()==True:
+	 		#query = "INSERT INTO product VALUES(NULL,?)"
+	 		#parameters=(self.service.get())
+			#self.run_query(query, parameters)
+        #else:
+            #print("Ponga el nombre del servicio")
+		#self.add_service()
+	
+	#Añadir productos
+	#def add_service(self):
+		#try:
+			#self.tree.item(self.tree.selection())["text"][0]
+		#except IndexError as e:
+			#self.message["text"] = "Por favor seleccione el servicio que desea agregar"
+			#return
+		#name = self.tree.item(self.tree.selection())["text"]
+		#query = "INSERT INTO Services SELECT FROM Product WHERE Nombre = ?"
+		#self.run_query(query,(name, ))
+		#self.message["text"] = "El servicio ha sido agregado correctamente"
+		#self.get_product
+	
+	
+	#Eliminar servicio
+	def delete_service(self):
+		try:
+			self.treec.item(self.treec.selection())["text"][0]
+		except IndexError as e:
+			self.message["text"] = "Por favor seleccione el servicio que desea eliminar"
+			return
+		Nombre = self.treec.item(self.treec.selection())["text"]
+		query = "DELETE FROM Services WHERE Nombre = ?"
+		self.run_query(query, (Nombre, ))
+		self.message["text"] = "El servicio ha sido eliminado correctamente"
+		self.get_service()
+	
 #Iniciando la aplicación
 if __name__=='__main__':
 	window= Tk()
