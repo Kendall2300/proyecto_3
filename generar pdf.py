ceaa1cbf9ef463192ffb276f7fe28aa16e44db26
Kendall2300 <kendallmartinezcarvajal4>
2020-08-11 13:12:29 -0600

    Añadido una corrección menor

--- generar pdf.py
+++ generar pdf.py
@@ -84,4 +84,4 @@ read_pdf = PyPDF2.PdfFileReader("samplefinish.pdf")
 number_of_pages = read_pdf.getNumPages()
 page = read_pdf.getPage(0)
 page_content = page.extractText()
-print page_content
+print (page_content)
