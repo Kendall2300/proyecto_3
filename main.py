f84f2a2a6cb6c0468104ad14ccd197e0d5308bfb
Kendall2300 <kendallmartinezcarvajal4>
2020-08-11 11:52:59 -0600

    Añadido el cambio de pantallas
    De momento solo se cuenta con dos pantallas una de servicios/compra y otra que sirve como ventana principal
    
    Co-Authored-By: gambo2907 <67343582+gambo2907@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,160 +1,142 @@
+
 from tkinter import ttk
 from tkinter import *
 import sqlite3
+import tkinter as tk
 
+class MainStream(tk.Tk):
+
+    def __init__(self,*args,**kwargs):
+
+        tk.Tk.__init__(self,*args,**kwargs)
+        container=tk.Frame(self)
+
+        container.pack(side="top",fill="both",expand=True)
+
+        container.grid_rowconfigure(0,weight=1)
+        container.grid_columnconfigure(0,weight=1)
+
+        self.frames = {}
+
+        for F in (second_menu, productos):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0,column=0,sticky="nsew")
+
+        self.show_frame(second_menu)
+
+    def show_frame(self,cont):
+
+        frame=self.frames[cont]
+        frame.tkraise()
+
+
+class second_menu(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        #label=tk.Label(self, text="Start Page")
+        #label.pack(pady=10,padx=10)
+
+
+        button_menu_services=tk.Button(self,text=
+        	"Contratar servicios",command=lambda:controller.show_frame(productos))
+
+        button_menu_services.pack(pady=10,padx=10)
+
+        #button_menu_recibos=tk.Button(self,text="Facturas Generadas",command=lambda:controller.show_frame())
+
+        #button_menu_recibos.pack()
 
 #Definiendo y creando la clase de productos/servicios a ofrecer
-class productos:
-
-	#Almacenando la base de datos dentro de la aplicación
+class productos(tk.Frame):
 	db_name = "database.db"
-	#Programando el inicio y display de la aplicación
-	def __init__(self,window):
-		self.wind=window
-		self.wind.title('3C Service Browser')
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self,parent)
+		#label = tk.Label(self, text="Start Page")
+		#label.pack(pady=10,padx=10)
+		button = tk.Button(self, text="Volver a la pagina principal", command=lambda: controller.show_frame(second_menu))
+		button.pack(pady=10,padx=10)
 
-		#Creando un contenedor
-		frame=LabelFrame(self.wind,text='Inserte el nombre del servicio\nque desea añadir')
-		frame.grid(row=0,column=0,columnspan=3,pady=20)
+		#Creando un los contendeores
+		big_box=LabelFrame(self)
+		big_box.pack(pady=15,padx=15)
+		lef_box=LabelFrame(self,big_box,text="Inserte el nombre de los servicios\nque desea añadir",labelanchor=N)
+		lef_box.pack(side=LEFT,pady=15,padx=15)
+		right_box=LabelFrame(self,big_box,text="Estos son los servicios que\nha añadido a su orden",labelanchor=N)
+		right_box.pack(side=RIGHT,pady=15,padx=15)
 
-		#Creo que de momento no se va a utilizar esto
-		# Label(frame,text='Nombre del producto').grid(row=1,column=0)
-		# self.name=Entry(frame)
-		# self.name.grid(row=1,column=1)
-		# self.name.focus()
+		#Introduccion de las id de los servicios
+		Label(lef_box,text="Servicio:").grid(row=1,column=0)
+		self.service_entry=Entry(lef_box)
+		self.service_entry.grid(row=1,column=1)
+		self.service_entry.focus()
 
-		#Introducción de las id de los servicios
-		Label(frame,text="Servicio: ").grid(row=1,column=0)
-		self.service=Entry(frame)
-		self.service.grid(row=1,column=1)
-		self.service.focus()
+		#Introduccion del precio por servicio
+		Label(lef_box,text="Precio:").grid(row=2,column=0)
+		self.price_entry=Entry(lef_box)
+		self.price_entry.grid(row=2,column=1)
 
-		#Introduccion del precio
-		Label(frame,text="Precio: ").grid(row=2,column=0)
-		self.price=Entry(frame)
-		self.price.grid(row=2,column=1)
+		#Botones de añadir servicios a la lista de compra
+		self.button_add_services=ttk.Button(lef_box,text="Añadir servicio",command=self.insertar_dato)
+		self.button_add_services.grid(row=3,columnspan=4,sticky=W+E,pady=10,padx=10)
 
-		#Boton de añadir servicio
-		self.bservicios = ttk.Button(frame,text='Anadir servicio',command=self.insertar_dato)
-		self.bservicios.grid(row=3,columnspan=3,sticky=W+E)
+		#Boton eliminar servicios?
 
-		#Botón de eliminar servicio
-		# self.delete = ttk.Button(frame, text = "Eliminar servicio",command=self.delete_service)
-		# self.delete.grid(row=4,columnspan=4,sticky=W+E)
-		
-		#Mensajes
-		self.message = Label(text = "", fg= "black")
-		self.message.grid(row=3, columnspan=5, sticky=W+E)
-		
 		#Creando la tabla de servicios con su respectivo id
-		self.tree=ttk.Treeview(height=10,columns=2)
-		self.tree.grid(row=1,column=0,columnspan=2,pady=5,padx=5)
-		self.tree.heading("#0", text="Nombre", anchor=CENTER)
-		self.tree.heading("#1", text="Precio en $", anchor=CENTER)
+		self.services_tree=ttk.Treeview(lef_box,height=10,columns=2)
+		self.services_tree.grid(row=4,column=0,columnspan=3,pady=20,padx=20)
+		self.services_tree.heading("#0",text="Nombre",anchor=CENTER)
+		self.services_tree.heading("#1",text="Precio en $", anchor=CENTER)
 
-		#
-		frame2=LabelFrame(self.wind,text="Estos son los servicios agregados\n a su pedido")
-		frame2.grid(row=0,column=3,columnspan=2, pady=20)
-		Label(frame2,text="").grid(row=1,column=3)
-		self.print=ttk.Button(frame2,text="Imprimir")
-		self.print.grid(row=1,columnspan=4,sticky=W+E)
+		#LABEL DE LADO DERECHO
+		Label(right_box)
+		self.print_button=ttk.Button(right_box,text="Imprimir")
+		self.print_button.grid(row=1,columnspan=3,sticky=W+E)
 
-		#Creando la tabla de servicios contratados
-		self.treec=ttk.Treeview(heigh=10,columns=2)
-		self.treec.grid(row=1,column=3,columnspan=2,pady=5,padx=5)
-		self.treec.heading("#0", text="Nombre", anchor=CENTER)
-		self.treec.heading("#1", text="Precio", anchor=CENTER)
+		#Tabla de servicios contratados
+		self.contratados_tree=ttk.Treeview(right_box,heigh=10,columns=2)
+		self.contratados_tree.grid(row=2,column=0,columnspan=3,pady=20,padx=20)
+		self.contratados_tree.heading("#0",text="Nombre",anchor=CENTER)
+		self.contratados_tree.heading("#1",text="Precio en $",anchor=CENTER)
 
 		#Colocando los productos en la tabla de servicios
-		self.get_product()
-		#self.get_service()
-	
+		self.get_products()
 	#Llamando a la base de datos
-	def run_query(self,query,parameters = ()):
-		with sqlite3.connect(self.db_name) as conn:
-			cursor = conn.cursor()
-			result = cursor.execute(query, parameters)
+	def run_query(self,query,parameters=()):
+		with sqlite3.connect(self.db_name)as conn:
+			cursor=conn.cursor()
+			result=cursor.execute(query,parameters)
 			conn.commit()
-		return result  
+		return result
 
 	#Obteniendo los productos de la base de datos
-	def get_product(self):
-		#Limpiando los datos que están en la tabla
-		records = self.tree.get_children()
-		for element in records:
-			self.tree.delete(element)
-		query = "SELECT * FROM Product ORDER BY Nombre DESC"
-		db_rows = self.run_query(query)
-		#Recorriendo la lista de productos para insertarlos en la tabla
+	def get_products(self):
+		#Limpiando la base de datos
+		records=self.services_tree.get_children()
+		for elements in records:
+			self.services_tree.delete(elements)
+		query="SELECT * FROM Product ORDER BY nombre DESC"
+		db_rows=self.run_query(query)
+		#Recorriendo la lista de productos para insertarlos en la tablla
 		for row in db_rows:
-			self.tree.insert("", 0, text= row[1],values=row[2])
-	
+			self.services_tree.insert("",0,text=row[1],values=row[2])
+
+	#Insertando los datos en la tabla de servicios contratados
 	def insertar_dato(self):
-		texto=[self.service.get()]
-		costo=[self.price.get()]
-		self.treec.insert("",0, text=texto[0],values=costo[0])
-
-	
+		texto=[self.service_entry.get()]
+		costo=[self.price_entry.get()]
+		self.contratados_tree.insert("",0,text=texto[0],values=costo[0])
 
 
 
 
-	# #Obtienendo la base de datos de la segunda tabla
-	# def get_service(self):
-	# 	#Limpiando los datos que están en la tabla
-	# 	records = self.treec.get_children()
-	# 	for element in records:
-	# 		self.treec.delete(element)
-	# 	query = "SELECT * FROM Services ORDER BY Nombre DESC"
-	# 	db_rows = self.run_query(query)
-	# 	#Recorriendo la lista de productos para insertarlos en la tabla
-	# 	for row in db_rows:
-	# 		self.treec.insert("", 0, text= row[1],values=row[2])
-	
-	#def validation(self):
-		#if len(self.service.get())!=0:
-	 		#return True
-		#else:
-            #return False
 
-	#def add_service(self):
-		#if self.validation()==True:
-	 		#query = "INSERT INTO product VALUES(NULL,?)"
-	 		#parameters=(self.service.get())
-			#self.run_query(query, parameters)
-        #else:
-            #print("Ponga el nombre del servicio")
-		#self.add_service()
-	
-	#Añadir productos
-	#def add_service(self):
-		#try:
-			#self.tree.item(self.tree.selection())["text"][0]
-		#except IndexError as e:
-			#self.message["text"] = "Por favor seleccione el servicio que desea agregar"
-			#return
-		#name = self.tree.item(self.tree.selection())["text"]
-		#query = "INSERT INTO Services SELECT FROM Product WHERE Nombre = ?"
-		#self.run_query(query,(name, ))
-		#self.message["text"] = "El servicio ha sido agregado correctamente"
-		#self.get_product
-	
-	
-	#Eliminar servicio
-	# def delete_service(self):
-	# 	try:
-	# 		self.treec.item(self.treec.selection())["text"][0]
-	# 	except IndexError as e:
-	# 		self.message["text"] = "Por favor seleccione el servicio que desea eliminar"
-	# 		return
-	# 	Nombre = self.treec.item(self.treec.selection())["text"]
-	# 	query = "DELETE FROM Services WHERE Nombre = ?"
-	# 	self.run_query(query, (Nombre ))
-	# 	self.message["text"] = "El servicio ha sido eliminado correctamente"
-	# 	self.get_service()
-	
+
+
 #Iniciando la aplicación
-if __name__=='__main__':
-	window= Tk()
-	application=productos(window)
-	window.mainloop()
+app= MainStream()
+app.mainloop()
