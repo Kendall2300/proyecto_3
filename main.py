4b1bb3e9867b6524dbbdb92c07ba1c97950df711
dlurena24 <daxr2d2@gmail.com>
2020-08-12 10:05:23 -0600

    V2.0.1

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ from tkinter import *
 def Register():
     print("*work in progress*")
 
-def Login():
 
+def Login():
     KNOWN_FACES_DIR = "./known_faces"
-    #UNKNOWN_FACES_DIR = "unknown_faces"
+    # UNKNOWN_FACES_DIR = "unknown_faces"
     TOLERANCE = 0.6
     FRAME_THIKNESS = 3
     FONT_THIKNESS = 2
-    MODEL = "cnn" #hog
+    MODEL = "cnn"  # hog
     video = cv2.VideoCapture(0)
-
 
     print("loading known faces")
 
@@ -24,21 +23,21 @@ def Login():
     known_names = []
 
     for name in os.listdir(KNOWN_FACES_DIR):
-        for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
-            image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
+        for filename in os.listdir(KNOWN_FACES_DIR):
+            image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{filename}")
             encoding = face_recognition.face_encodings(image)[0]
             known_faces.append(encoding)
             known_names.append(name)
 
     print("procesing unknown faces")
     while True:
-        #print(filename)
-        #image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+        # print(filename)
+        # image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
         ret, image = video.read()
 
-        locations = face_recognition.face_locations(image, model = MODEL)
+        locations = face_recognition.face_locations(image, model=MODEL)
         encodings = face_recognition.face_encodings(image, locations)
-        #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         for face_encoding, face_location in zip(encodings, locations):
             results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
@@ -53,15 +52,16 @@ def Login():
                 cv2.rectangle(image, top_left, bottom_right, color, FRAME_THIKNESS)
 
                 top_left = (face_location[3], face_location[2])
-                bottom_right = (face_location[1], face_location[2]+22)
+                bottom_right = (face_location[1], face_location[2] + 22)
                 cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-                cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200,200,200), FONT_THIKNESS)
+                cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            (200, 200, 200), FONT_THIKNESS)
 
         cv2.imshow(filename, image)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-        #cv2.waitKey(20000)
-        #cv2.destroyWindow(filename)
+        # cv2.waitKey(20000)
+        # cv2.destroyWindow(filename)
 
 
 def starting_screen():
@@ -69,12 +69,14 @@ def starting_screen():
     screen = Tk()
     screen.geometry("300x250")
     screen.title("Inicio de sesion")
-    Label(screen, text = "Inicio de sesion", bg = "grey", width = "300", height = "2", font = ("Calibri", 13)).pack()
-    Label(text = "").pack
-    Button(text="Login", height="2", width = "30", command =Login).pack()
+    Label(screen, text="Inicio de sesion", bg="grey", width="300", height="2", font=("Calibri", 13)).pack()
+    Label(text="").pack
+    Button(text="Login", height="2", width="30", command=Login).pack()
     Label(text="").pack
     Button(text="Register", height="2", width="30", command=Register).pack()
 
     screen.mainloop()
+
+
 starting_screen()
 
