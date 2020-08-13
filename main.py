bfd4dba0eb27588ab7e465d9093972dce1ac3a97
Kendall2300 <kendallmartinezcarvajal4>
2020-08-13 17:49:58 -0600

    Intento de merge final. Falta el arreglo de las funciones para calculo de subtotal y total
    Co-Authored-By: pasifae600 <65846967+pasifae600@users.noreply.github.com>

--- main.py
+++ main.py
@@ -125,6 +125,41 @@ class productos(tk.Frame):
 		return subtotal
 		print(subtotal)
 
+	#Print
+	def print(self):
+		URL_WSAA = "login url"
+		URL_WSFEv1 = "service screen url"
+		CUIT = 20267565393
+		CERT = "../reingart.crt"
+		PRIVATEKEY = "../reingart.key"
+		CACHE = "../cache"
+		CONF_PDF = dict(LOGO="../screen/logo.png",EMPRESA="Ejemplo de empresa",MEMBRETE1="Ejemplo de direccion",MEMBRETE2="Capital Federal",CUIT="CUIT 30-00000000-0",IIBB="exento",IVA="IVA Responsable Inscripto",INICIO="Inicio de Actividad: fecha")
+
+
+		def facturar(registros):
+			#login
+
+			if face_login ==(false):
+				"return to login"
+			else:
+				"build bill"
+				fepdf = FEPDF()
+				fepdf.CargarFormato("sample.pdf")
+				fepdf.FmtCantidad = "0.2"
+				fepdf.FmtPrecio = "0.2"
+				fepdf.CUIT = CUIT
+				for k, v in CONF_PDF.items():
+					fepdf.AgregarDato(k, v)
+			"registri data for bill + pdf generation"
+			for reg in 'registros':
+				hoy = datetime.date.today().strftime("%Y%m%d")
+				cbte = Comprobante(tipo_cbte=6, punto_vta=4000, fecha_cbte=hoy,cbte_nro=reg.get("nro"),tipo_doc=96, nro_doc=reg["dni"],nombre_cliente=reg["nombre"],domicilio_cliente=reg["domicilio"],fecha_serv_desde=reg.get("periodo_desde"),fecha_serv_hasta=reg.get("periodo_hasta"),fecha_venc_pago=reg.get("venc_pago", hoy))
+				cbte.agregar_item(ds=reg["descripcion"],qty=reg.get("cantidad", 1),precio=reg.get("precio", 0),tasa_iva=reg.get("tasa_iva", 21.))
+				print("Factura autorizada")
+				ok = cbte.generar_pdf(fepdf, "samplefinish.pdf".format(nro))
+				print("PDF generado", ok)
+
+
 	#Llamando a la base de datos
 	def run_query(self,query,parameters=()):
 		with sqlite3.connect(self.db_name)as conn:
